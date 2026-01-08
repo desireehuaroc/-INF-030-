0f2d7f8ea79b54c94e58c4e0706d06dba0007747
desireehuaroc <desire.huaroc@ucb.edu.bo>
2026-01-08 12:25:23 -0400

    python

---
+++ primer_codigodepython.py
@@ -0,0 +1,15 @@
+print("Hola, Mundo!")
+
+x = 5
+y = 10
+resultado = x + y 
+print("Hola, Desi el resultado es: ")
+print(resultado)
+
+print("----------------------------------")
+
+print("Calcular el area de un rectangulo")
+base = 10.55
+altura = 57.09
+area_rectangulo = base * altura
+print(area_rectangulo)
